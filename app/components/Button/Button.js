bcaf3a7207fa5b770947ca0b6966e1ef9475fe61
james <jamesdejesus74@gmail.com>
2025-10-05 18:41:19 +0800

    added plop for file generation

---
+++ app/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const Button = () => {
+  return (
+    <div className="button">
+      <h1>Button Component</h1>
+    </div>
+  );
+};
+
+export default Button;
